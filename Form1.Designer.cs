b8b76b1e95efe2f503c7088c9a991eeede71e61e
mini <dezz1985@gmail.com>
2020-04-28 18:07:34 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace BookGeneratinon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.dataSet1 = new System.Data.DataSet();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.Number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.T_Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Т_Tema = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Mesto_Work = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Adress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.T_Index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Numb_ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Number,
+            this.T_Name,
+            this.Т_Tema,
+            this.Mesto_Work,
+            this.Adress,
+            this.T_Index,
+            this.Numb_});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(714, 208);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(716, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Открыть файл *.xls";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(714, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Собрать книгу";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(3, 30);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(714, 178);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(2, 465);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(722, 21);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Location = new System.Drawing.Point(2, 30);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Size = new System.Drawing.Size(722, 433);
+            this.splitContainer1.SplitterDistance = 216;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // Number
+            // 
+            this.Number.FillWeight = 53.29949F;
+            this.Number.HeaderText = "№";
+            this.Number.Name = "Number";
+            // 
+            // T_Name
+            // 
+            this.T_Name.FillWeight = 107.7834F;
+            this.T_Name.HeaderText = "ФИО";
+            this.T_Name.Name = "T_Name";
+            // 
+            // Т_Tema
+            // 
+            this.Т_Tema.FillWeight = 107.7834F;
+            this.Т_Tema.HeaderText = "Тема статьи";
+            this.Т_Tema.Name = "Т_Tema";
+            // 
+            // Mesto_Work
+            // 
+            this.Mesto_Work.FillWeight = 107.7834F;
+            this.Mesto_Work.HeaderText = "Место работы";
+            this.Mesto_Work.Name = "Mesto_Work";
+            // 
+            // Adress
+            // 
+            this.Adress.FillWeight = 107.7834F;
+            this.Adress.HeaderText = "Адрес";
+            this.Adress.Name = "Adress";
+            // 
+            // T_Index
+            // 
+            this.T_Index.FillWeight = 107.7834F;
+            this.T_Index.HeaderText = "Индекс(почтовый)";
+            this.T_Index.Name = "T_Index";
+            // 
+            // Numb_
+            // 
+            this.Numb_.FillWeight = 107.7834F;
+            this.Numb_.HeaderText = "Номер()";
+            this.Numb_.Name = "Numb_";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 489);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Сборщик книг";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn T_Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Т_Tema;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Mesto_Work;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Adress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn T_Index;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Numb_;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExcelObj = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
+using System.IO;
+using System.Threading;
+namespace BookGeneratinon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog ofd = new OpenFileDialog();
+            //Задаем расширение имени файла по умолчанию.
+            ofd.DefaultExt = "*.xls;*.xlsx";
+            //Задаем строку фильтра имен файлов, которая определяет
+            //варианты, доступные в поле "Файлы типа" диалогового
+            //окна.
+            ofd.Filter = "Excel Sheet(*.xlsx)|*.xlsx";
+            //Задаем заголовок диалогового окна.
+            ofd.Title = "Выберите документ для загрузки данных";
+            ExcelObj.Application app = new ExcelObj.Application();
+            ExcelObj.Workbook workbook;
+            ExcelObj.Worksheet NwSheet;
+            ExcelObj.Range ShtRange;
+            DataTable dt = new DataTable();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+               // textBox1.Text = ofd.FileName;
+
+                workbook = app.Workbooks.Open(ofd.FileName);
+
+                //Устанавливаем номер листа из котрого будут извлекаться данные
+                //Листы нумеруются от 1
+                NwSheet = (ExcelObj.Worksheet)workbook.Sheets.get_Item(1);
+                ShtRange = NwSheet.UsedRange;
+
+                for (int Cnum = 1; Cnum <= dataGridView1.Columns.Count; Cnum++)
+                {
+                    dt.Columns.Add(dataGridView1.Columns[Cnum].HeaderText);
+                }
+                dt.AcceptChanges();
+
+                //string[] columnNames = new String[dt.Columns.Count];
+                //for (int i = 0; i < dt.Columns.Count; i++)
+                //{
+                //    columnNames[0] = dt.Columns[i].ColumnName;
+                //}
+
+                for (int Rnum = 2; Rnum <= ShtRange.Rows.Count; Rnum++)
+                {
+                    DataRow dr = dt.NewRow();
+                    for (int Cnum = 1; Cnum <= ShtRange.Columns.Count; Cnum++)
+                    {
+                        if ((ShtRange.Cells[Rnum, Cnum] as ExcelObj.Range).Value2 != null)
+                        {
+                            dr[Cnum-1] = (ShtRange.Cells[Rnum, Cnum] as ExcelObj.Range).Value2.ToString();
+                        }
+                    }
+                   dt.Rows.Add(dr);
+                    dt.AcceptChanges();
+                }
+                dataSet1.Tables.Add(dt);
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                dataGridView1.DataSource = dataSet1.Tables[0];
+
+                //dataGridView1.DataMember = 
+                dataGridView1.Refresh();
+                workbook.Close();
+                app.Quit();
+            }
+            else
+                Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = dataSet1.Tables[0].Rows.Count;
+            backgroundWorker1.RunWorkerAsync();
+            //doc_res.Close();
+            //app_res.Quit();
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string result = string.Empty;
+            Word.Application app_res = new Word.Application();
+            Word.Document doc_res = new Word.Document();
+            app_res.Visible = false;
+            
+            app_res.DisplayAlerts = Word.WdAlertLevel.wdAlertsNone;
+            object nullobject = System.Reflection.Missing.Value;
+            Object pathToSaveObj = Directory.GetCurrentDirectory() + "\\Result.docx";
+            doc_res.SaveAs(ref pathToSaveObj, Word.WdSaveFormat.wdFormatDocumentDefault, ref nullobject, ref nullobject, ref nullobject,
+                ref nullobject, false, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject,
+                ref nullobject, ref nullobject, ref nullobject, ref nullobject);
+            
+            if (!File.Exists(Directory.GetCurrentDirectory() + "\\Result.docx"))
+            {
+                doc_res = app_res.Documents.Add(ref nullobject, ref nullobject, ref nullobject, ref nullobject);
+            }
+            else
+            {
+                doc_res = app_res.Documents.Open(Directory.GetCurrentDirectory() + "\\Result.docx");
+            }
+            vOutput("Создали выходной файл " + Directory.GetCurrentDirectory() + "\\Result.docx"+Environment.NewLine);
+            int i = 0;
+            foreach (DataRow row in dataSet1.Tables[0].Rows)
+            {
+          
+                string filename = string.Format("orig{0}", row.ItemArray[0].ToString());
+
+                string[] f = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\docs", filename + "*");
+
+                //Word.Application wordObject = new Word.Application();
+                object File = f[0];
+
+                Word.Application wordobject = new Word.Application();
+                wordobject.DisplayAlerts = Word.WdAlertLevel.wdAlertsNone;
+                Word.Document docs= new Word.Document();
+                //wordobject.Visible = false;
+                try
+                {
+                    // docs = wordobject.Documents.Open(ref File, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject);
+
+                    //docs.ActiveWindow.Selection.WholeStory();
+                    //docs.ActiveWindow.Selection.Copy();
+                    //Thread.Sleep(500);
+                    // object temp = Clipboard.GetData("");
+                    Microsoft.Office.Interop.Word.Paragraph para1 = doc_res.Content.Paragraphs.Add(ref nullobject);
+                    // object styleHeading1 = "Заголовок 2";
+                    //para1.Range.set_Style(styleHeading1);
+                    para1.Range.InsertParagraph();
+                    para1.Range.Font.Size = 14;
+                    para1.Range.Font.Name = "Times New Roman";
+                    para1.Format.Alignment = Word.WdParagraphAlignment.wdAlignParagraphRight;
+                    para1.Range.Text = row.ItemArray[1].ToString();
+                    para1.Range.InsertParagraphAfter();
+                    doc_res.Save();
+                    para1.Range.Font.Size = 12;
+                    para1.Range.Font.Name = "Times New Roman";
+                    para1.Format.Alignment = Word.WdParagraphAlignment.wdAlignParagraphRight;
+                    //para1.Range.Text = row.ItemArray[1].ToString();
+                    para1.Range.Text = row.ItemArray[3].ToString();
+                    para1.Range.InsertParagraphAfter();
+                    doc_res.Save();
+
+
+                    para1.Range.Font.Size = 16;
+                    para1.Range.Font.Name = "Times New Roman";
+                    para1.Format.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    para1.Range.Font.Bold = 1;
+                    para1.Range.Text = row.ItemArray[2].ToString();
+
+                    para1.Range.InsertParagraphAfter();
+                    doc_res.Save();
+
+                    ////object missing = Missing.Value;
+                    //object what = Word.WdGoToItem.wdGoToLine;
+                    //object which = Word.WdGoToDirection.wdGoToLast;
+                    //Word.Range endRange = doc_res.GoTo(ref what, ref which, ref nullobject, ref nullobject);
+                    //para1.Range.Paste();
+                    app_res.Selection.GoTo(Word.WdGoToItem.wdGoToLine, Word.WdGoToDirection.wdGoToLast, ref nullobject, ref nullobject);
+                    app_res.Selection.InsertFile(f[0]);
+                    //doc_res.ActiveWindow.Selection.Paste();
+                    Thread.Sleep(200);
+
+                    doc_res.Save();
+                    string output = string.Format("Добавили файл {0} в выходной файл", f[0]);
+                    vOutput(output + Environment.NewLine);
+                    //richTextBox1.Text += Environment.NewLine + Environment.NewLine + row.Cells[1].Value.ToString() + Environment.NewLine + row.Cells[3].Value.ToString() + Environment.NewLine + row.Cells[2].Value.ToString() + Environment.NewLine;
+                    // richTextBox1.Paste();
+                    // rtfMain.Document.Paste();
+                   // Thread.Sleep(200);
+                    docs.Close(ref nullobject, ref nullobject, ref nullobject);
+                    //Thread.Sleep(200);
+                    wordobject.Quit(ref nullobject, ref nullobject, ref nullobject);
+                   // Thread.Sleep(200);
+
+                    backgroundWorker1.ReportProgress(i);
+                    i++;
+                }
+                catch (Exception error)
+                {
+                    docs.Close();
+                    wordobject.Quit(ref nullobject, ref nullobject, ref nullobject);
+                   // Thread.Sleep(200);
+                    //wordobject.Quit(ref nullobject, ref nullobject, ref nullobject);
+                    vOutput("Проблема с файлом: " + f[0] + "   " + error.Message+ Environment.NewLine);
+                    backgroundWorker1.ReportProgress(i);
+                    i++;
+                    //return;
+                    //throw error;
+                }
+
+            }
+            //Object pathToSaveObj = Directory.GetCurrentDirectory() + "\\Result.docx";
+            //doc_res.SaveAs(ref pathToSaveObj, Word.WdSaveFormat.wdFormatDocumentDefault, ref nullobject, ref nullobject, ref nullobject, 
+            //    ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject,
+            //    ref nullobject, ref nullobject, ref nullobject, ref nullobject);
+            app_res.Visible = true;
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            dataGridView1.Rows[e.ProgressPercentage].Selected = true;
+        }
+        delegate void SetTextCallback(string text);
+        public void vOutput(string signaltime)
+        {
+            try
+            {
+                if (this.richTextBox1.InvokeRequired)
+                {
+                    SetTextCallback d = new SetTextCallback(vOutput);
+                    this.Invoke(d, new object[] { signaltime });
+                }
+                else
+                {
+                    this.richTextBox1.AppendText(signaltime);
+                    
+
+
+
+                    this.richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                    this.richTextBox1.ScrollToCaret();
+                }
+            }
+            catch (SystemException ex)
+            {
+                string es = ex.Message;
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar1.Visible = false;
+        }
+    }
+}
